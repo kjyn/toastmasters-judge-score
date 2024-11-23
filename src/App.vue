c4811cfb2f1016671c5ea14bfc3e3afa5695ae1b
kjyn <el.shirom08@gmail.com>
2024-11-23 18:41:35 +0900

    タイトル追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
+import PageTitle from './components/PaageTitle.vue'
 import ScoreSheet from './components/ScoreSheet.vue'
 import RankTable from './components/RankTable.vue'
 import { ref } from 'vue'
@@ -9,18 +10,23 @@ const totalResults = ref([])
 
 <template>
   <header>
-
-    <div class="container text-center">
+    <div class="container-fluid text-center">
       <div class="row">
-          <div class="col-md-8">
-            <ScoreSheet @response="(result) => totalResults = result" />
-          </div>
-          <div class="col-md-4">
-            <RankTable :totalResults="totalResults"/>
-          </div>
+        <PageTitle></PageTitle>
       </div>
     </div>
   </header>
+
+  <div class="container-fluid text-center">
+    <div class="row">
+        <div class="col-md-8">
+          <ScoreSheet @response="(result) => totalResults = result" />
+        </div>
+        <div class="col-md-4">
+          <RankTable :totalResults="totalResults"/>
+        </div>
+    </div>
+  </div>
 
   <!-- <RouterView /> -->
 </template>
---
+++ src/components/PaageTitle.vue
@@ -0,0 +1,4 @@
+<template>
+    <h3>Score Sheet</h3>
+    <h6>For Judging The Toastmasters Interntional Contest</h6>
+</template>
