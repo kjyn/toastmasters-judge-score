e566fe808ec3330a6f248e23d0c046e773b206a9
kjyn <el.shirom08@gmail.com>
2025-03-23 19:48:15 +0900

    ページタイトルを微調整

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,4 +1,25 @@
 <template>
-    <h3>Score Sheet</h3>
-    <h6>For Judging The Toastmasters Interntional Contest</h6>
+    <div class="container-fluid text-center">
+        <h3>Score Sheet</h3>
+        <h6>For Judging The Toastmasters Interntional Contest</h6>
+    </div>
 </template>
+
+<style scoped>
+    div .container-fluid {
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    h3 {
+        font-size: 1.5rem;
+        font-weight: bold;
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+    h6 {
+        font-size: 1rem;
+        font-weight: normal;
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+</style>
