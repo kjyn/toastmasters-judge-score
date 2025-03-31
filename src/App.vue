755abfcfb4c7c13e7a621db09c22a5a66714551d
kjyn <el.shirom08@gmail.com>
2025-04-01 01:34:12 +0900

    合計金額受け渡し方法の変更

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-// import { RouterLink, RouterView } from 'vue-router'
-import PageTitle from './components/PaageTitle.vue'
+import PageTitle from './components/PageTitle.vue'
 import ScoreSheet from './components/ScoreSheet.vue'
 import RankTable from './components/RankTable.vue'
 import { ref } from 'vue'
@@ -20,15 +19,13 @@ const totalResults = ref([])
   <div class="container-fluid text-center">
     <div class="row">
         <div class="col-md-8">
-          <ScoreSheet @response="(result) => totalResults = result" />
+          <ScoreSheet @response="(result) => totalResults.value = result" />
         </div>
         <div class="col-md-4">
           <RankTable :totalResults="totalResults"/>
         </div>
     </div>
   </div>
-
-  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
